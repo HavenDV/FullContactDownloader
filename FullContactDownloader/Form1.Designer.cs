5c00c16f1babc26dfe93a6dd193f97b27aa64d7f
Konstantin S. <havendv@gmail.com>
2017-01-17 22:19:50 +1000

    Initial commit

--- FullContactDownloader/Form1.Designer.cs
+++ FullContactDownloader/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.ConsoleRichBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(12, 250);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(429, 23);
+            this.DownloadButton.TabIndex = 0;
+            this.DownloadButton.Text = "DownloadButton";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // ConsoleRichBox
+            // 
+            this.ConsoleRichBox.Location = new System.Drawing.Point(12, 12);
+            this.ConsoleRichBox.Name = "ConsoleRichBox";
+            this.ConsoleRichBox.Size = new System.Drawing.Size(429, 232);
+            this.ConsoleRichBox.TabIndex = 1;
+            this.ConsoleRichBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 285);
+            this.Controls.Add(this.ConsoleRichBox);
+            this.Controls.Add(this.DownloadButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.RichTextBox ConsoleRichBox;
     }
 }
 
--- FullContactDownloader/Form1.cs
+++ FullContactDownloader/Form1.cs
@@ -1,20 +1,147 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
 
 namespace FullContactDownloader
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        private async void DownloadButton_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-FullContact-APIKey", "97a086b3aac1b0a5");
+            var response = await client.GetAsync(
+                "https://api.fullcontact.com/v2/company/lookup.json?domain=fullcontact.com");
+            var responseContent = response.Content;
+            using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+            {
+                var company = JsonConvert.DeserializeObject<Company>(await reader.ReadToEndAsync());
+                ConsoleRichBox.AppendText(company.languageLocale + Environment.NewLine);
+            }
+        }
+    }
+
+    public class Category
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+
+    public class EmailAddress
+    {
+        public string value { get; set; }
+        public string label { get; set; }
+    }
+
+    public class PhoneNumber
+    {
+        public string number { get; set; }
+        public string label { get; set; }
+    }
+
+    public class Region
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Country
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+    }
+
+    public class Address
+    {
+        public string addressLine1 { get; set; }
+        public string addressLine2 { get; set; }
+        public string locality { get; set; }
+        public Region region { get; set; }
+        public Country country { get; set; }
+        public string postalCode { get; set; }
+        public string label { get; set; }
+    }
+
+    public class ContactInfo
+    {
+        public List<EmailAddress> emailAddresses { get; set; }
+        public List<PhoneNumber> phoneNumbers { get; set; }
+        public List<Address> addresses { get; set; }
+    }
+
+    public class Link
+    {
+        public string url { get; set; }
+        public string label { get; set; }
+    }
+
+    public class Image
+    {
+        public string url { get; set; }
+        public string label { get; set; }
+    }
+
+    public class Organization
+    {
+        public string name { get; set; }
+        public int approxEmployees { get; set; }
+        public string founded { get; set; }
+        public string overview { get; set; }
+        public ContactInfo contactInfo { get; set; }
+        public List<Link> links { get; set; }
+        public List<Image> images { get; set; }
+        public List<string> keywords { get; set; }
+    }
+
+    public class SocialProfile
+    {
+        public string bio { get; set; }
+        public int followers { get; set; }
+        public int following { get; set; }
+        public string typeId { get; set; }
+        public string typeName { get; set; }
+        public string url { get; set; }
+        public string username { get; set; }
+        public string id { get; set; }
+    }
+
+    public class TopCountryRanking
+    {
+        public int rank { get; set; }
+        public string locale { get; set; }
+    }
+
+    public class Ranking
+    {
+        public int rank { get; set; }
+        public string locale { get; set; }
+    }
+
+    public class Traffic
+    {
+        public List<TopCountryRanking> topCountryRanking { get; set; }
+        public List<Ranking> ranking { get; set; }
+    }
+
+    public class Company
+    {
+        public int status { get; set; }
+        public string requestId { get; set; }
+        public List<Category> category { get; set; }
+        public string logo { get; set; }
+        public string website { get; set; }
+        public string languageLocale { get; set; }
+        public Organization organization { get; set; }
+        public List<SocialProfile> socialProfiles { get; set; }
+        public Traffic traffic { get; set; }
     }
 }
